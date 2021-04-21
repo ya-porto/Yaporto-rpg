fd0bcca497e272d42bab015fb036c25b3e9e4a21
Mihail Gaisin <miroga64@gmail.com>
2021-04-21 22:03:34 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+          { from: "./public/fonts", to: "fonts" },
+          {from: "./public/images", to: "images"}
+      ]   
+      },
+  ),
+  ]
+}; 
