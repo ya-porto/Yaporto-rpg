da855f57d550a64fe1b48a4a2d7edaf90ce2553c
Mihail Gaisin <miroga64@gmail.com>
2021-04-26 21:43:30 +0500

    Base Mechanics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,119 @@
 import React, {Component} from 'react';
+import GameCanvas from './game/canvasGenerate'
+import Character from './game/character'
+import lvlGenerate from './game/lvlgenerate'
 
 import './App.css';
+const canvasImg = new Image();
+canvasImg.src = '/public/images/bg.jpeg'
+
+const charImg = new Image()
+charImg.src = '/public/images/golem/Golem_01.png'
+charImg.width = 63;
+charImg.height = 64;
+
+const charImgA1 = new Image()
+charImgA1.src = '/public/images/golem/Golem_01.png'
+charImgA1.width = 63;
+charImgA1.height = 64;
+
+const charImgA2 = new Image()
+charImgA2.src = '/public/images/golem/Golem_02.png'
+charImgA2.width = 63;
+charImgA2.height = 64;
+
+const charImgA3 = new Image()
+charImgA3.src = '/public/images/golem/Golem_03.png'
+charImgA3.width = 63;
+charImgA3.height = 64;
+
+const charImgA4 = new Image()
+charImgA4.src = '/public/images/golem/Golem_04.png'
+charImgA4.width = 63;
+charImgA4.height = 64;
+
+const charImgA5 = new Image()
+charImgA5.src = '/public/images/golem/Golem_05.png'
+charImgA5.width = 63;
+charImgA5.height = 64;
+
+const charImgA6 = new Image()
+charImgA6.src = '/public/images/golem/Golem_06.png'
+charImgA6.width = 63;
+charImgA6.height = 64;
+
+const charImgA7 = new Image()
+charImgA7.src = '/public/images/golem/Golem_07.png'
+charImgA7.width = 63;
+charImgA7.height = 64;
+
+const charImgA8 = new Image()
+charImgA8.src = '/public/images/golem/Golem_08.png'
+charImgA8.width = 63;
+charImgA8.height = 64;
+
+const charImgA9 = new Image()
+charImgA9.src = '/public/images/golem/Golem_09.png'
+charImgA9.width = 63;
+charImgA9.height = 64;
+
+
+
+const lvlMatrix = [
+	[['C'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*']],
+	[[],['*'],['*'],['*'],['*'],[],[],[],[],[],[],[],[]],
+	[[],[],[],[],[],[],[],[],[],[],[],[],[]],
+	[[],[],[],[],[],[],[],[],[],[],[],[],[]],
+	[['*'],[],[],[],[],[],[],[],[],[],[],[],[]],
+	[['*'],['*'],[],[],[],[],[],[],[],[],[],[],[]],
+	[['*'],['*'],['*'],[],[],[],[],[],[],[],[],[],[]],
+	[['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],['*'],[],[],[],[]]
+]
+
+let isActive = false
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1 className="mt-5">Привет мир!</h1>
-			</div>
+			<GameCanvas x_p={828} y_p={508} image={canvasImg} mass={[]}/>
 		);
 	}
 }
+setTimeout(()=>{
+	lvlGenerate(lvlMatrix, 65, 64, 'canvas')
+	const charAttack = [
+		charImgA1, charImgA2, charImgA3, charImgA4, charImgA5, charImgA6, charImgA7, charImgA8, charImgA9
+	]
+	console.log(charImgA1)
+	const char1 = new Character(0, 0, charImg, 'canvas', charAttack)
+	document.addEventListener('keydown', function(event){
+
+			isActive = true
+			if(event.code == 'ArrowUp' || event.code == 'KeyW'){
+				char1.moveCharacter('up', lvlMatrix, 65, 64)
+				return;
+			}
+		
+			if(event.code == 'ArrowDown' || event.code == 'KeyS'){
+				char1.moveCharacter('down', lvlMatrix, 65, 64)
+				return;
+			}
+		
+			if(event.code == 'ArrowRight' || event.code == 'KeyD'){
+				char1.moveCharacter('right', lvlMatrix, 65, 64)
+				return;
+			}
+		
+			if(event.code == 'ArrowLeft' || event.code == 'KeyA'){
+				char1.moveCharacter('left', lvlMatrix, 65, 64)
+				return;
+			}
+	
+			if(event.code == 'KeyX'){
+				let time = performance.now();
+				char1.attackCharacter(time, 500)
+				return;
+			}
+    })
+}, 100)
 export default App;
---
+++ src/components/game/character.tsx
@@ -0,0 +1,110 @@
+
+export default class Character{
+    ctx: CanvasRenderingContext2D | null;
+    image:HTMLImageElement;
+    prev_x:number;
+    prev_y:number;
+    attackMass: Array<HTMLImageElement>;
+    attackFrame: number;
+    startTime: number; 
+    animationTime: number;
+    attackId: number;
+
+    constructor(start_x:number, start_y:number, image:HTMLImageElement, canvas_id:string, attackMass: Array<HTMLImageElement>){
+        this.attackFrame = 0;
+        const canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
+        if(canvas){
+            this.ctx = canvas.getContext('2d')
+        }
+        this.attackMass = attackMass
+        this.image = image
+        this.prev_x = start_x;
+        this.prev_y = start_y;
+
+        setTimeout(() => {
+            if(this.ctx){
+                this.ctx.beginPath();
+                this.ctx.drawImage(image, start_x, start_y, this.image.width, this.image.height)
+                this.ctx.closePath();
+            }
+        }, 10)
+    }
+
+    moveCharacter(direction:string, mass: Array<Array<Array<string>>>, x_size: number, y_size: number, images:HTMLImageElement | null = null){
+        if(this.ctx){
+            let clear_x = this.prev_x;
+            let clear_y = this.prev_y
+           
+            switch(direction){
+                case 'up':
+                    this.prev_y -= 64;
+                    break;
+    
+                case 'down':
+                    this.prev_y += 64;
+                    break;
+    
+                case 'right':
+                    this.prev_x += 65;
+                    break;
+    
+                
+                case 'left':
+                    this.prev_x -= 65;
+                    break;
+            }
+    
+            if(images != null){
+                this.image = images
+            }
+            console.log(this.prev_y / y_size, this.prev_x / x_size)
+            if(mass[this.prev_y / y_size][this.prev_x / x_size][0] == undefined){
+                this.ctx.clearRect(clear_x, clear_y, this.image.width, this.image.height)
+                this.ctx.beginPath();
+                this.ctx.drawImage(this.image, this.prev_x, this.prev_y, this.image.width, this.image.height)
+                this.ctx.closePath();
+                delete mass[clear_y / y_size][clear_x / x_size][0]
+                mass[this.prev_y / y_size][this.prev_x / x_size][0] = 'C'
+            }else{
+                this.prev_x = clear_x;
+                this.prev_y = clear_y
+            }
+        }
+    }
+
+    attackCharacter(startTime: number, animationTime: number){
+        this.startTime = startTime;
+        this.animationTime = animationTime;
+        this.attackId = window.setInterval(() => {
+            this.attackAnimate()
+        }, animationTime / 8)
+        
+    }
+
+    attackAnimate(){
+        if(this.ctx){
+            this.ctx.beginPath();
+            this.ctx.clearRect(this.prev_x, this.prev_y, this.image.width, this.image.height)
+            const time = performance.now();
+            const shiftTime = time - this.startTime;
+            const multiply = shiftTime / this.animationTime;
+            this.ctx.drawImage(this.attackMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
+            console.log(this.attackMass[this.attackFrame])
+            this.ctx.closePath();
+
+            if(multiply < 1){
+                this.attackFrame++;
+                this.attackAnimate;
+            }else{
+                clearInterval(this.attackId)
+                this.attackFrame = 0;
+                this.ctx.beginPath();
+                this.ctx.clearRect(this.prev_x, this.prev_y, this.image.width, this.image.height)
+                this.ctx.drawImage(this.attackMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
+                this.ctx.closePath();
+            }
+        }
+ 
+    }
+}
+
