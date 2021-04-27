63cee3862d2ebf70e41bb517315ad211cc2798df
Mihail Gaisin <miroga64@gmail.com>
2021-04-27 19:36:05 +0500

    Added hp, dmg, armor, attack, die, deal damage, get damaged

---
+++ src/components/game/attackSprite.tsx
@@ -0,0 +1,50 @@
+const charImgA1 = new Image()
+charImgA1.src = '/public/images/golem/Attack/Golem_01.png'
+charImgA1.width = 63;
+charImgA1.height = 64;
+
+const charImgA2 = new Image()
+charImgA2.src = '/public/images/golem/Attack/Golem_02.png'
+charImgA2.width = 63;
+charImgA2.height = 64;
+
+const charImgA3 = new Image()
+charImgA3.src = '/public/images/golem/Attack/Golem_03.png'
+charImgA3.width = 63;
+charImgA3.height = 64;
+
+const charImgA4 = new Image()
+charImgA4.src = '/public/images/golem/Attack/Golem_04.png'
+charImgA4.width = 63;
+charImgA4.height = 64;
+
+const charImgA5 = new Image()
+charImgA5.src = '/public/images/golem/Attack/Golem_05.png'
+charImgA5.width = 63;
+charImgA5.height = 64;
+
+const charImgA6 = new Image()
+charImgA6.src = '/public/images/golem/Attack/Golem_06.png'
+charImgA6.width = 63;
+charImgA6.height = 64;
+
+const charImgA7 = new Image()
+charImgA7.src = '/public/images/golem/Attack/Golem_07.png'
+charImgA7.width = 63;
+charImgA7.height = 64;
+
+const charImgA8 = new Image()
+charImgA8.src = '/public/images/golem/Attack/Golem_08.png'
+charImgA8.width = 63;
+charImgA8.height = 64;
+
+const charImgA9 = new Image()
+charImgA9.src = '/public/images/golem/Attack/Golem_09.png'
+charImgA9.width = 63;
+charImgA9.height = 64;
+
+const charAttack = [
+    charImgA1, charImgA2, charImgA3, charImgA4, charImgA5, charImgA6, charImgA7, charImgA8, charImgA9
+]
+
+export default charAttack
--- src/components/game/character.tsx
+++ src/components/game/character.tsx
@@ -1,4 +1,3 @@
-
 export default class Character{
     ctx: CanvasRenderingContext2D | null;
     image:HTMLImageElement;
@@ -9,8 +8,27 @@ export default class Character{
     startTime: number; 
     animationTime: number;
     attackId: number;
+    hp: number;
+    attack: number;
+    armor: number;
+    deathMass: Array<HTMLImageElement>;
+    x_size: number;
+    y_size: number;
+    canvasMatrix:  Array<Array<Array<string>>>;
+    x_position: number;
+    y_position: number;
+    isDead: boolean;
+    numberId: number;
 
-    constructor(start_x:number, start_y:number, image:HTMLImageElement, canvas_id:string, attackMass: Array<HTMLImageElement>){
+    constructor(start_x:number, start_y:number, image:HTMLImageElement, canvas_id:string, attackMass: Array<HTMLImageElement>, deathMass:  Array<HTMLImageElement> = [], hp: number = 100, attack: number = 10, armor: number = 0,  canvasMatrix: Array<Array<Array<string>>>, x_size: number, y_size: number,){
+        this.hp = hp;
+        this.isDead = false;
+        this.canvasMatrix = canvasMatrix;
+        this.x_size = x_size;
+        this.y_size = y_size;
+        this.deathMass = deathMass
+        this.attack = attack;
+        this.armor = armor;
         this.attackFrame = 0;
         const canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
         if(canvas){
@@ -20,51 +38,127 @@ export default class Character{
         this.image = image
         this.prev_x = start_x;
         this.prev_y = start_y;
+        this.y_position = this.prev_y / this.y_size
+        this.x_position = this.prev_x / this.x_size
 
         setTimeout(() => {
             if(this.ctx){
                 this.ctx.beginPath();
+                this.ctx.fillStyle = "#FFF";
+                this.ctx.strokeStyle = "#FFF";
+                this.ctx.strokeText(this.hp.toString(), start_x + 25, start_y + 10);
                 this.ctx.drawImage(image, start_x, start_y, this.image.width, this.image.height)
                 this.ctx.closePath();
             }
         }, 10)
     }
 
-    moveCharacter(direction:string, mass: Array<Array<Array<string>>>, x_size: number, y_size: number, images:HTMLImageElement | null = null){
+    updateCharacter( hp: number = this.hp, attack: number = this.attack, armor: number = this.armor){
+        this.hp = hp;
+        this.attack = attack;
+        this.armor = armor;  
+    }
+
+    getPosition(){
+        return [this.y_position, this.x_position ]
+    }
+
+    getParams(){
+        return {
+            'hp': this.hp,
+            'attack': this.attack,
+            'armor': this.armor
+        }
+    }
+
+    getDamage(dmg: number){
+        if(!this.isDead){
+            this.hp = this.hp - (dmg - this.armor)
+            if(this.hp <= 0){
+                this.isDead = true
+                this.hp = 0;
+                let time = performance.now();
+                this.deathCharacter(time, 700)
+                return 'die'
+            }
+    
+            if(this.ctx){
+                this.ctx.beginPath();
+                this.ctx.clearRect(this.prev_x, this.prev_y, this.image.width, this.image.height)
+                this.ctx.strokeText(this.hp.toString(), this.prev_x + 25, this.prev_y + 10);
+                this.ctx.drawImage(this.attackMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
+                this.ctx.closePath();
+            }
+        }
+    }
+
+    deathCharacter(startTime: number, animationTime: number){
+        this.startTime = startTime;
+        this.animationTime = animationTime;
+        this.attackId = window.setInterval(() => {
+            this.deathAnimate()
+        }, animationTime / 14)
+    }
+
+    deathAnimate(){
+        if(this.ctx){
+            this.ctx.beginPath();
+            this.ctx.clearRect(this.prev_x, this.prev_y, this.image.width, this.image.height)
+            const time = performance.now();
+            const shiftTime = time - this.startTime;
+            const multiply = shiftTime / this.animationTime;
+            this.ctx.drawImage(this.deathMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
+            this.ctx.closePath();
+
+            if(multiply < 1){
+                this.attackFrame++;
+                this.attackAnimate;
+            }else{
+                clearInterval(this.attackId)
+                this.attackFrame = 0;
+            }
+        }
+    }
+
+
+    moveCharacter(direction:string, images:HTMLImageElement | null = null){
         if(this.ctx){
             let clear_x = this.prev_x;
             let clear_y = this.prev_y
            
             switch(direction){
                 case 'up':
-                    this.prev_y -= 64;
+                    this.prev_y -= this.y_size;
                     break;
     
                 case 'down':
-                    this.prev_y += 64;
+                    this.prev_y += this.y_size;
                     break;
     
                 case 'right':
-                    this.prev_x += 65;
+                    this.prev_x += this.x_size;
                     break;
     
                 
                 case 'left':
-                    this.prev_x -= 65;
+                    this.prev_x -= this.x_size;
                     break;
             }
     
             if(images != null){
                 this.image = images
             }
-            if(mass[this.prev_y / y_size][this.prev_x / x_size][0] == undefined){
+            if(this.canvasMatrix[this.prev_y / this.y_size][this.prev_x / this.x_size][0] == undefined){
+                this.x_position = this.prev_x / this.x_size;
+                this.y_position = this.prev_y / this.y_size;
                 this.ctx.clearRect(clear_x, clear_y, this.image.width, this.image.height)
                 this.ctx.beginPath();
+                this.ctx.strokeText(this.hp.toString(), this.prev_x + 25, this.prev_y + 10);
                 this.ctx.drawImage(this.image, this.prev_x, this.prev_y, this.image.width, this.image.height)
                 this.ctx.closePath();
-                mass[clear_y / y_size][clear_x / x_size].splice(0, 1)
-                mass[this.prev_y / y_size][this.prev_x / x_size][0] = 'C'
-                console.log(mass)
+                this.canvasMatrix[clear_y / this.y_size][clear_x / this.x_size].splice(0, this.canvasMatrix[clear_y / this.y_size][clear_x / this.x_size].length)
+                this.canvasMatrix[this.y_position][this.x_position][0] = 'C'
+
             }else{
                 this.prev_x = clear_x;
                 this.prev_y = clear_y
@@ -88,8 +182,9 @@ export default class Character{
             const time = performance.now();
             const shiftTime = time - this.startTime;
             const multiply = shiftTime / this.animationTime;
+            this.ctx.strokeText(this.hp.toString(), this.prev_x + 25, this.prev_y + 10);
+
             this.ctx.drawImage(this.attackMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
-            console.log(this.attackMass[this.attackFrame])
             this.ctx.closePath();
 
             if(multiply < 1){
@@ -100,6 +195,7 @@ export default class Character{
                 this.attackFrame = 0;
                 this.ctx.beginPath();
                 this.ctx.clearRect(this.prev_x, this.prev_y, this.image.width, this.image.height)
+                this.ctx.strokeText(this.hp.toString(), this.prev_x + 25, this.prev_y + 10);
                 this.ctx.drawImage(this.attackMass[this.attackFrame], this.prev_x, this.prev_y, this.image.width, this.image.height);
                 this.ctx.closePath();
             }
