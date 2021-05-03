04a3e5628f57a2730b63ec63ce1e58142226128d
ammodit-p <ammodit@yandex.ru>
2021-05-03 15:47:46 +0300

    убрала данные для дебага

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import './styles/style.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App';
-import {GameShop} from './pages/gameshop/gameshop';
-// import {Inventory} from './pages/inventory/index';
-import {mockShopItems} from './mocks/mockShopItems';
+import App from './components/App';
 
-ReactDOM.render(<GameShop clothes={mockShopItems}/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
