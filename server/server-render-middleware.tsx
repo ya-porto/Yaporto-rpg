789b8f2191429b6355bedf605f242d1324aa2b4c
Егор <skeetch2010@gmail.com>
2021-05-30 13:49:17 +0400

    Сделал ssr router. Страница GAME не работает

--- server/server-render-middleware.tsx
+++ server/server-render-middleware.tsx
@@ -1,29 +1,43 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import React from 'react';
+import {StaticRouter} from 'react-router-dom';
+import {StaticRouterContext} from 'react-router';
 import {renderToStaticMarkup, renderToString} from 'react-dom/server';
-import App from '../src/components/App';
+import {App} from '../src/components/App';
 
 function makeHTMLPage(content: string) {
-    // Тут мы создаем страницу, которую будем раздавать
-    const html =  renderToStaticMarkup(
-        <html lang="ru">
-            <head>
-                <title>From SSR with Love</title>
-                <link rel='stylesheet' href='./css/style.css'></link>
-            </head>
-            <body>
-                {/* Сюда вставляем срендеренное в строку содержание страницы*/}
-                <main id="app" dangerouslySetInnerHTML={{__html: content}} />
-            </body>
-        </html>,
-    );
+	// Тут мы создаем страницу, которую будем раздавать
+	const html = renderToStaticMarkup(
+		<html lang="ru">
+			<head>
+				<title>From SSR with Love</title>
+				<link rel="stylesheet" href="./css/style.css"></link>
+			</head>
+			<body>
+				{/* Сюда вставляем срендеренное в строку содержание страницы */}
+				<main id="app" dangerouslySetInnerHTML={{__html: content}} />
+			</body>
+		</html>
+	);
 
-    return `<!doctype html>${html}`
-  }
+	return `<!doctype html>${html}`;
+}
 
 export default (req: Request, res: Response) => {
-    const appContent = (<App />)
-    // Здесь рендерим в строку jsx-компонент
-    const reactDom = renderToString(appContent);
-    res.send(makeHTMLPage(reactDom));
-}
+	const location = req.url;
+	const context: StaticRouterContext = {};
+	const appContent = (
+		<StaticRouter context={context} location={location}>
+			<App />
+		</StaticRouter>
+	);
+	// Здесь рендерим в строку jsx-компонент
+	const reactDom = renderToString(appContent);
+
+	if (context.url) {
+		res.redirect(context.url);
+		return;
+	}
+
+	res.send(makeHTMLPage(reactDom));
+};
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {hot} from 'react-hot-loader/root';
+
+import {Forum, Thread} from '../pages/forum';
+import {GameShop} from '../pages/gameshop';
+import {Inventory} from '../pages/inventory';
+import {Leaderboard} from '../pages/leaderboard';
+import {Main} from '../pages/main';
+import {Profile} from '../pages/profile';
+import {Signin} from '../pages/signin';
+import {Signup} from '../pages/signup';
+import {Game} from '../pages/game';
 
 import './App.css';
+import {requireAuthentication} from './authentication';
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1 className="mt-5">Привет мир!</h1>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/gameshop" component={requireAuthentication(GameShop)}></Route>
+				<Route exact path="/signin" component={Signin}></Route>
+				<Route exact path="/signup" component={Signup}></Route>
+				<Route exact path="/game" component={Game}></Route>
+				<Route exact path="/inventory" component={requireAuthentication(Inventory)}></Route>
+				<Route exact path="/forum" component={requireAuthentication(Forum)}></Route>
+				<Route path="/forum/thread" component={requireAuthentication(Thread)}></Route>
+				<Route path="/leaderboard" component={Leaderboard}></Route>
+				<Route path="/profile" component={requireAuthentication(Profile)}></Route>
+				<Route path="/home" component={Main}></Route>
+				<Redirect from="/" to="/home"></Redirect>
+			</Switch>
+		</div>
+	);
 }
-export default App;
+
+const Component = hot(App);
+
+export {Component as App};
