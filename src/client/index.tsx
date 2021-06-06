c2478130b423d4b0a626dfc077121c9346d5b55e
Егор <skeetch2010@gmail.com>
2021-06-06 22:42:24 +0400

    Добавил dev mode вебпака и запуск его. Попутные правки пакетов. Начал делать hot reloading (пока без обновления страницы)

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -9,21 +9,24 @@ import {createReduxStore} from '../redux/rootStore';
 import {State} from '../redux/types';
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import {IS_DEV} from '../../webpack/env';
 
 const {store} = createReduxStore(window.__INITIAL_STATE__);
 
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (reg) {
-		if (reg.installing) {
-			console.log('Service worker installing');
-		} else if (reg.waiting) {
-			console.log('Service worker installed');
-		} else if (reg.active) {
-			console.log('Service worker active');
-		}
-	}).catch(function (error) {
-		console.log('Registration failed with ' + error);
-	});
+if (!IS_DEV) {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (reg) {
+			if (reg.installing) {
+				console.log('Service worker installing');
+			} else if (reg.waiting) {
+				console.log('Service worker installed');
+			} else if (reg.active) {
+				console.log('Service worker active');
+			}
+		}).catch(function (error) {
+			console.log('Registration failed with ' + error);
+		});
+	}
 }
 
 declare global {
